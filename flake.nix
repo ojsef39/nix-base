83486cee53396124d8f34b6f785160c7f2f0c13a
Josef <me@jhofer.de>
2024-11-03 02:42:13 +0100

    chore: comment out linux

--- flake.nix
+++ flake.nix
@@ -4,16 +4,12 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
+    darwin.url = "github:lnl7/nix-darwin/master";
   };
 
-  outputs = { self, nixpkgs, home-manager }: {
+  outputs = { self, nixpkgs, home-manager, darwin }: {
     sharedModules = {};
-
-    macModules = {
-      programs.zsh = {
-        enable = true;
-      };
-    };
+    macModules = {};
     linuxModules = {};
   };
 }
