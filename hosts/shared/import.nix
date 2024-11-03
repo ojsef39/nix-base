a729dcc96709cd8d9f4e90b44d51d0411de804aa
Josef <me@jhofer.de>
2024-11-03 15:26:33 +0100

    try agan

--- hosts/shared/import.nix
+++ hosts/shared/import.nix
@@ -1,23 +1,18 @@
-{ vars, pkgs, ... }:
-let
-  # Get all immediate subdirectories of ./programs
-  programDirs = builtins.attrNames (
-    builtins.readDir ./programs
-  );
-  # Map each directory to its default.nix path
-  programModules = map (dir: ./programs/${dir}/default.nix) programDirs;
+sharedModules = [
+  ./nix/core.nix
+  home-manager.darwinModules.home-manager
+  ./hosts/shared/import.nix
 
-  # Determine home directory based on system
-  homeDirectory = builtins.getEnv "HOME";
-in
-{
-  nixpkgs.config.allowUnfree = true;
-
-  programs.home-manager.enable = true;
-
-  imports = programModules;
-
-  home = {
-    inherit homeDirectory;
-  };
-}
+  {
+    home-manager = {
+      useGlobalPkgs = true;
+      useUserPackages = true;
+      extraSpecialArgs = {
+        inputs = {
+          inherit nixpkgs home-manager darwin;
+        };
+      };
+      users.${builtins.getEnv "USER"} = import ./home;
+    };
+  }
+];
