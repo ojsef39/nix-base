3386b456e4eb8e033f7b4bd1759975806ae6b3e5
Josef <me@jhofer.de>
2024-11-03 17:43:28 +0100

    maybe this

--- flake.nix
+++ flake.nix
@@ -5,19 +5,18 @@
     home-manager.url = "github:nix-community/home-manager";
     darwin.url = "github:lnl7/nix-darwin/master";
   };
-  outputs = inputs @ { self, nixpkgs, home-manager, darwin, ... }:
-  {
+  outputs = inputs @ { self, nixpkgs, home-manager, darwin, ... }: {
     sharedModules = [
       ./nix/core.nix
       home-manager.darwinModules.home-manager
-      { config, ... }: {
+      ({ config, ... }: {
         home-manager = {
           useGlobalPkgs = true;
           useUserPackages = true;
           extraSpecialArgs = { inherit inputs; };
           users.${config.vars.user} = import ./hosts/shared/import.nix;
         };
-      }
+      })
     ];
     macModules = [
       ./hosts/darwin/import.nix
