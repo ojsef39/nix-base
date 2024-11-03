be9559efa6a6e13e45d4ed1a2ad1dd70a51409cb
Josef <me@jhofer.de>
2024-11-03 18:25:33 +0100

    tjos

--- flake.nix
+++ flake.nix
@@ -5,18 +5,20 @@
     home-manager.url = "github:nix-community/home-manager";
     darwin.url = "github:lnl7/nix-darwin/master";
   };
-  outputs = inputs @ { self, nixpkgs, home-manager, darwin, ... }: {
+  outputs = inputs @ { self, nixpkgs, home-manager, darwin, ... }:
+  {
     sharedModules = [
       ./nix/core.nix
       home-manager.darwinModules.home-manager
-      ({ config, ... }: {
+      ({ vars, ... }):
+      {
         home-manager = {
           useGlobalPkgs = true;
           useUserPackages = true;
           extraSpecialArgs = { inherit inputs; };
-          users.${config.vars.user} = import ./hosts/shared/import.nix;
+          users.${vars.user} = import ./hosts/shared/import.nix;
         };
-      })
+      }
     ];
     macModules = [
       ./hosts/darwin/import.nix
