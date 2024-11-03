b0cf1e33540b13f14c64c21ae493a1dde8850d1c
Josef <me@jhofer.de>
2024-11-03 16:49:12 +0100

    accept vars

--- flake.nix
+++ flake.nix
@@ -1,13 +1,11 @@
 {
   description = "ojsef39 base nix configuration";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     darwin.url = "github:lnl7/nix-darwin/master";
   };
-
-  outputs = { self, nixpkgs, home-manager, darwin }: {
+  outputs = { self, nixpkgs, home-manager, darwin, ... }: {
     sharedModules = [
       ./nix/core.nix
       home-manager.darwinModules.home-manager
@@ -19,11 +17,12 @@
             inputs = {
               inherit nixpkgs home-manager darwin;
             };
+            vars = self.vars;
           };
-          users.${vars.user} = import ./hosts/shared/import.nix;
+          users.${self.vars.user} = import ./hosts/shared/import.nix { inherit vars; };
         };
       }
-      ./hosts/shared/import.nix
+      ./hosts/shared/import.nix { inherit vars; }
     ];
     macModules = [
       ./hosts/darwin/import.nix
