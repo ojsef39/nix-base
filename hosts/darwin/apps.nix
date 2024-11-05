f4a37da050648072849d601ea8154728ee3dc498
Josef <me@jhofer.de>
2024-11-05 21:03:19 +0100

    move file first maybe?

--- hosts/darwin/apps.nix
+++ hosts/darwin/apps.nix
@@ -1,5 +1,10 @@
 { pkgs, lib, vars, ... }: {
 
+  home.file."Library/Preferences/com.googlecode.iterm2.plist" = {
+    source = ./apps/iterm2/com.googlecode.iterm2.plist;
+    target = "link";
+  };
+
   home.packages = with pkgs; [
     # CLI utilities
     # _1password-cli  # Password manager
@@ -13,9 +18,4 @@
     raycast         # Spotlight replacement
     utm             # Virtualization
   ];
-
-  home-manager.backupFileExtension = "backup";
-  home.file."Library/Preferences/com.googlecode.iterm2.plist" = {
-    source = ./apps/iterm2/com.googlecode.iterm2.plist;
-  };
 }
