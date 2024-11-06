20b8372cc5baad970db971e17cc65ef46d4f85e4
Josef <me@jhofer.de>
2024-11-06 18:13:41 +0100

    try .copy

--- hosts/darwin/apps.nix
+++ hosts/darwin/apps.nix
@@ -13,25 +13,8 @@
     utm             # Virtualization
   ];
 
-  home.activation = {
-    copyITerm2Prefs = lib.hm.dag.entryAfter ["writeBoundary"] ''
-      plist_path="$HOME/Library/Preferences/com.googlecode.iterm2.plist"
-      backup_path="$HOME/Library/Preferences/com.googlecode.iterm2.plist.backup"
-
-      # Remove old backup if it exists
-      if [ -f "$backup_path" ]; then
-        $DRY_RUN_CMD sudo rm "$backup_path"
-      fi
-
-      # Backup current plist if it exists
-      if [ -f "$plist_path" ]; then
-        $DRY_RUN_CMD sudo mv "$plist_path" "$backup_path"
-      fi
-
-      # Copy and set up the new file
-      $DRY_RUN_CMD sudo cp ${./apps/iterm2/com.googlecode.iterm2.plist} "$plist_path"
-      $DRY_RUN_CMD sudo chmod 600 "$plist_path"
-      $DRY_RUN_CMD sudo chown $USER "$plist_path"
-    '';
+  home.file.copy."Library/Preferences/com.googlecode.iterm2.plist" = {
+    source = ./apps/iterm2/com.googlecode.iterm2.plist;
+    target = "Library/Preferences/com.googlecode.iterm2.plist";
   };
 }
