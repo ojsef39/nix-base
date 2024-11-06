e713d088303bb44d7144c22cb31a105f2a39bba8
Josef <me@jhofer.de>
2024-11-06 18:06:01 +0100

    fixx iterm2

--- hosts/darwin/apps.nix
+++ hosts/darwin/apps.nix
@@ -13,8 +13,25 @@
     utm             # Virtualization
   ];
 
-  home.file."Library/Preferences/com.googlecode.iterm2.plist" = {
-    source = ./apps/iterm2/com.googlecode.iterm2.plist;
-    target = "Library/Preferences/com.googlecode.iterm2.plist";
+  home.activation = {
+    copyITerm2Prefs = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      plist_path="$HOME/Library/Preferences/com.googlecode.iterm2.plist"
+      backup_path="$HOME/Library/Preferences/com.googlecode.iterm2.plist.backup"
+
+      # Remove old backup if it exists
+      if [ -f "$backup_path" ]; then
+        $DRY_RUN_CMD sudo rm "$backup_path"
+      fi
+
+      # Backup current plist if it exists
+      if [ -f "$plist_path" ]; then
+        $DRY_RUN_CMD sudo mv "$plist_path" "$backup_path"
+      fi
+
+      # Copy and set up the new file
+      $DRY_RUN_CMD sudo cp ${./apps/iterm2/com.googlecode.iterm2.plist} "$plist_path"
+      $DRY_RUN_CMD sudo chmod 600 "$plist_path"
+      $DRY_RUN_CMD sudo chown $USER "$plist_path"
+    '';
   };
 }
