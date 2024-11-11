e221fe7ed927bc630e45cc04a3bba095694f6cf6
Josef Hofer <me@jhofer.de>
2024-11-11 21:02:02 +0100

    chore: template project_selector

--- hosts/shared/programs/kitty/default.nix
+++ hosts/shared/programs/kitty/default.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, vars, ... }:
 
 {
   programs.kitty = {
@@ -158,6 +158,37 @@
     "kitty/scripts" = {
       source = ./scripts;
       recursive = true;
+    };
+
+    "kitty/scripts/project_selector.sh" = {
+      text = ''
+        #!/bin/bash
+        project_dirs=(${vars.project_selector})
+        # Display the subfolders of the project directories
+        projects() {
+          for dir in "$project_dirs[@]"; do
+            find "$dir" -maxdepth 1 -type d | tail -n +2 # List subdirectories only, excluding the base directory itself
+          done
+        }
+        # Select a project directory using fzf
+        project_selector() {
+          local project
+          project=$(projects | fzf)
+
+          if [ -n "$project" ]; then
+            {
+              kitten @ launch --type=tab --cwd="$project" nvim .
+              echo "Changed to $project"
+            } || {
+              echo "Failed to change directory to $project"
+              exit 1
+            }
+          else
+            echo "No project selected."
+          fi
+        }
+        project_selector
+      '';
     };
   };
 }
