623315798ad38e0034251a6684d7cec8c79d5850
Josef Hofer <josef.hofer@1und1.de>
2024-11-21 15:54:27 +0100

    Revert "feat: add helm stable repo"
    This reverts commit b0c2518eaa4e15b03b6710d3ebfb5160cc00b925.

--- hosts/shared/home-manager/k8/default.nix
+++
@@ -1,9 +0,0 @@
-{
-  home.activation = {
-    ## HELM
-    helmSetup = ''
-      /opt/homebrew/bin/helm repo add stable https://charts.helm.sh/stable
-      /opt/homebrew/bin/helm repo update
-    '';
-  };
-}
