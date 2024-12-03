25f8f490406de563302506fc01f24040fcaeee9f
Josef Hofer <josef.hofer@1und1.de>
2024-12-03 10:32:02 +0100

    chore: add ssh configs

--- hosts/shared/home-manager/ssh/default.nix
+++ hosts/shared/home-manager/ssh/default.nix
@@ -6,6 +6,8 @@
       "*" = {
         extraOptions = {
           IdentityAgent = ''"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"'';
+          UseKeychain = "yes";
+          AddKeysToAgent = "yes";
         };
       };
     };
