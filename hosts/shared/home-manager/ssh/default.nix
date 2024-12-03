e0035adf59207d2d29410ae441a19628702069df
Josef <me@jhofer.de>
2024-12-03 11:08:06 +0100

    chore: update ssh config (#32)
    Co-authored-by: Josef Hofer <josef.hofer@1und1.de>

--- hosts/shared/home-manager/ssh/default.nix
+++ hosts/shared/home-manager/ssh/default.nix
@@ -8,6 +8,7 @@
           IdentityAgent = ''"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"'';
           UseKeychain = "yes";
           AddKeysToAgent = "yes";
+          UseKeychain = "yes";
         };
       };
     };
