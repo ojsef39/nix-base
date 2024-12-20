ec2deaae38df7cb0bfe567d546cf99c49fb533f9
Josef Hofer <josef.hofer@1und1.de>
2024-12-20 15:47:46 +0100

    fix(hm-ghq): lib undefined

--- hosts/shared/home-manager/ghq/default.nix
+++ hosts/shared/home-manager/ghq/default.nix
@@ -1,9 +1,11 @@
+{ pkgs, lib, ... }:
 {
-  ## GHQ -> This must run after the writeBoundary to make sure gitconfig with ghq settings is set
+  ## GHQ -> This must run after the linkGeneration to make sure gitconfig with ghq settings is set
   home.activation = {
-    ghqGetRepos = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      ghq get https://github.com/ojsef39/commit-oracle
-      ghq get https://github.com/ojsef39/nix-base
+    ghqGetRepos = lib.hm.dag.entryAfter [ "linkGeneration" ] ''
+      export PATH=$PATH:/usr/bin
+      ${pkgs.ghq}/bin/ghq get https://github.com/ojsef39/commit-oracle
+      ${pkgs.ghq}/bin/ghq get https://github.com/ojsef39/nix-base
     '';
   };
 }
