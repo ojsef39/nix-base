be2932e99142b9402f195d443ccfe4d8e599fa1c
Josef Hofer <josef.hofer@1und1.de>
2024-12-20 16:30:50 +0100

    chore(hm-ghq): improve ghq

--- hosts/shared/home-manager/ghq/default.nix
+++ hosts/shared/home-manager/ghq/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ pkgs, lib, vars, ... }:
 {
   ## GHQ -> This must run after the linkGeneration to make sure gitconfig with ghq settings is set
   home.activation = {
@@ -6,6 +6,13 @@
       export PATH=$PATH:/usr/bin
       ${pkgs.ghq}/bin/ghq get https://github.com/ojsef39/commit-oracle
       ${pkgs.ghq}/bin/ghq get https://github.com/ojsef39/nix-base
+      export PATH=$PATH:/bin/hostname
+      hostname=$(/bin/hostname)
+      if [[ $hostname == L???-* ]]; then
+        ${pkgs.ghq}/bin/ghq get https://${vars.git.url}/${vars.user}/nix-work
+      else
+        ${pkgs.ghq}/bin/ghq get https://github.com/ojsef39/nix-personal
+      fi
     '';
   };
 }
