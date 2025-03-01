4d5c4afe02e308acadf06792deac24819385d760
Josef <me@jhofer.de>
2025-03-01 13:48:59 +0100

    chore(sys-stylix): comment out wallpaper (#63)
    * fix(nix): move nixpkgs.config to correct file
    
    * chore(sys-stylix): comment out wallpaper

--- hosts/darwin/stylix.nix
+++ hosts/darwin/stylix.nix
@@ -6,6 +6,6 @@
     autoEnable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
     opacity.terminal = 0.9;
-    image = ./wallpaper.png;
+    # image = ./wallpaper.png;
   };
 }
