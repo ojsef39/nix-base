39ee65ae52f59d09828fcb131187e15e6a97b4b6
Josef <me@jhofer.de>
2025-03-22 15:52:34 +0100

    feat: add k9s (#74)

---
+++ hosts/shared/home-manager/k9s/default.nix
@@ -0,0 +1,29 @@
+{ pkgs, lib, vars, ... }:
+{
+  programs.k9s = {
+    enable = lib.mkDefault true;
+    settings = {
+      k9s = {
+        ui = {
+          skin = "catppuccin-macchiato";
+          enableMouse = true;
+          reactive = true;
+        };
+        logger = {
+          tail = 500;
+          buffer = 5000;
+          sinceSeconds = -1;
+        };
+      };
+    };
+  };
+  home.file = {
+    "Library/Application Support/k9s/skins/catppuccin-macchiato.yaml" = {
+      source = builtins.fetchurl {
+        url = "https://raw.githubusercontent.com/catppuccin/k9s/refs/heads/main/dist/catppuccin-macchiato.yaml";
+        sha256 = "1wdxway40xzz0kl4phs64h0h9b4xvkgsh7c75w0s9za8az6bf79r";
+      };
+      force = true;
+    };
+  };
+}
