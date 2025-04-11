6db49421245e6c07e2a7052220d1e8ab03b6b219
Josef Hofer <me@jhofer.de>
2025-04-11 14:58:15 +0200

    chore: reenable nixocord

--- hosts/shared/home-manager/nixcord/default.nix
+++ hosts/shared/home-manager/nixcord/default.nix
@@ -11,106 +11,106 @@ let
     else "${config.xdg.configHome}/vesktop/themes/${themeFile}";
 in {
   stylix.targets.vesktop.enable = false; # Deactivate stylix because it doesnt work on macos
-  # programs.nixcord = {
-  #   enable = true;
-  #   discord = {
-  #     enable = true;
-  #     vencord = {
-  #       enable = false;
-  #       package = pkgs.vencord;
-  #     };
-  #   };
-  #   vesktop = {
-  #     enable = true;
-  #     package = pkgs.vesktop.overrideAttrs (previousAttrs: {
-  #       patches = previousAttrs.patches ++ [
-  #         (pkgs.fetchpatch {
-  #           name = "micfix-68c19662909621f421bc4a896e9225e21d62b3ed.patch";
-  #           url = "https://gist.githubusercontent.com/ojsef39/b8d8190008869b8a868b998494e3f95d/raw/68c19662909621f421bc4a896e9225e21d62b3ed/micfix.patch";
-  #           sha256 = "sha256-orMoR0NmHKirNG/6qEr35gjKzkMjHltgkOzioo6gIfY=";
-  #         })
-  #       ];
-  #     });
-  #   };
-  #   config = {
-  #     useQuickCss = false;
-  #     disableMinSize = true;
-  #     frameless = true;
-  #     enabledThemes = [ themeFile ];
-  #     plugins = {
-  #       alwaysAnimate.enable = true;
-  #       betterFolders = {
-  #         enable = true;
-  #         sidebar = false;
-  #         sidebarAnim = true;
-  #         closeAllFolders = true;
-  #         closeAllHomeButton = true;
-  #         forceOpen = true;
-  #       };
-  #       anonymiseFileNames = {
-  #         enable = true;
-  #         anonymiseByDefault = true;
-  #
-  #       };
-  #       appleMusicRichPresence.enable = true;
-  #       betterGifPicker.enable = true;
-  #       betterRoleContext.enable = true;
-  #       betterSettings.enable = true;
-  #       betterUploadButton.enable = true;
-  #       biggerStreamPreview.enable = true;
-  #       callTimer.enable = true;
-  #       clearURLs.enable = true;
-  #       customIdle = {
-  #         enable = true;
-  #         idleTimeout = 5.0;
-  #         remainInIdle = true;
-  #       };
-  #       crashHandler.enable = true;
-  #       experiments = {
-  #         enable = true;
-  #         toolbarDevMenu = true;
-  #       };
-  #       favoriteEmojiFirst.enable = true;
-  #       favoriteGifSearch.enable = true;
-  #       fixImagesQuality.enable = true;
-  #       fullSearchContext.enable = true;
-  #       gameActivityToggle.enable = true;
-  #       imageZoom = {
-  #         enable = true;
-  #         size = 200.0;
-  #       };
-  #       memberCount.enable = true;
-  #       mentionAvatars.enable = true;
-  #       messageClickActions.enable = true;
-  #       messageLatency.enable = true;
-  #       messageLinkEmbeds.enable = true;
-  #       noDevtoolsWarning.enable = true;
-  #       noF1.enable = true;
-  #       noOnboardingDelay.enable = true;
-  #       permissionsViewer.enable = true;
-  #       pinDMs.enable = true;
-  #       plainFolderIcon.enable = true;
-  #       previewMessage.enable = true;
-  #       quickMention.enable = true;
-  #       readAllNotificationsButton.enable = true;
-  #       reverseImageSearch.enable = true;
-  #       sendTimestamps.enable = true;
-  #       summaries.enable = true;
-  #       shikiCodeblocks = {
-  #         enable = true;
-  #         theme = "https://raw.githubusercontent.com/shikijs/textmate-grammars-themes/refs/heads/main/packages/tm-themes/themes/catppuccin-macchiato.json";
-  #       };
-  #       translate.enable = true;
-  #       typingIndicator.enable = true;
-  #       typingTweaks.enable = true;
-  #       validReply.enable = true;
-  #       viewRaw.enable = true;
-  #       voiceChatDoubleClick.enable = true;
-  #       webScreenShareFixes.enable = true;
-  #       whoReacted.enable = true;
-  #     };
-  #   };
-  # };
+  programs.nixcord = {
+    enable = true;
+    discord = {
+      enable = true;
+      vencord = {
+        enable = false;
+        package = pkgs.vencord;
+      };
+    };
+    vesktop = {
+      enable = true;
+      package = pkgs.vesktop.overrideAttrs (previousAttrs: {
+        patches = previousAttrs.patches ++ [
+          (pkgs.fetchpatch {
+            name = "micfix-68c19662909621f421bc4a896e9225e21d62b3ed.patch";
+            url = "https://gist.githubusercontent.com/ojsef39/b8d8190008869b8a868b998494e3f95d/raw/68c19662909621f421bc4a896e9225e21d62b3ed/micfix.patch";
+            sha256 = "sha256-orMoR0NmHKirNG/6qEr35gjKzkMjHltgkOzioo6gIfY=";
+          })
+        ];
+      });
+    };
+    config = {
+      useQuickCss = false;
+      disableMinSize = true;
+      frameless = true;
+      enabledThemes = [ themeFile ];
+      plugins = {
+        alwaysAnimate.enable = true;
+        betterFolders = {
+          enable = true;
+          sidebar = false;
+          sidebarAnim = true;
+          closeAllFolders = true;
+          closeAllHomeButton = true;
+          forceOpen = true;
+        };
+        anonymiseFileNames = {
+          enable = true;
+          anonymiseByDefault = true;
+
+        };
+        appleMusicRichPresence.enable = true;
+        betterGifPicker.enable = true;
+        betterRoleContext.enable = true;
+        betterSettings.enable = true;
+        betterUploadButton.enable = true;
+        biggerStreamPreview.enable = true;
+        callTimer.enable = true;
+        clearURLs.enable = true;
+        customIdle = {
+          enable = true;
+          idleTimeout = 5.0;
+          remainInIdle = true;
+        };
+        crashHandler.enable = true;
+        experiments = {
+          enable = true;
+          toolbarDevMenu = true;
+        };
+        favoriteEmojiFirst.enable = true;
+        favoriteGifSearch.enable = true;
+        fixImagesQuality.enable = true;
+        fullSearchContext.enable = true;
+        gameActivityToggle.enable = true;
+        imageZoom = {
+          enable = true;
+          size = 200.0;
+        };
+        memberCount.enable = true;
+        mentionAvatars.enable = true;
+        messageClickActions.enable = true;
+        messageLatency.enable = true;
+        messageLinkEmbeds.enable = true;
+        noDevtoolsWarning.enable = true;
+        noF1.enable = true;
+        noOnboardingDelay.enable = true;
+        permissionsViewer.enable = true;
+        pinDMs.enable = true;
+        plainFolderIcon.enable = true;
+        previewMessage.enable = true;
+        quickMention.enable = true;
+        readAllNotificationsButton.enable = true;
+        reverseImageSearch.enable = true;
+        sendTimestamps.enable = true;
+        summaries.enable = true;
+        shikiCodeblocks = {
+          enable = true;
+          theme = "https://raw.githubusercontent.com/shikijs/textmate-grammars-themes/refs/heads/main/packages/tm-themes/themes/catppuccin-macchiato.json";
+        };
+        translate.enable = true;
+        typingIndicator.enable = true;
+        typingTweaks.enable = true;
+        validReply.enable = true;
+        viewRaw.enable = true;
+        voiceChatDoubleClick.enable = true;
+        webScreenShareFixes.enable = true;
+        whoReacted.enable = true;
+      };
+    };
+  };
 
   # Download theme file
   home.file = {
