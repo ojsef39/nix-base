6dc47dcf2d66ce8f42ab56b3941216fba44c7974
Josef <me@jhofer.de>
2025-04-22 14:52:58 +0200

    fix(hm-editor): keybinds and build no longer working (#86)
    Co-authored-by: Josef Hofer <josef.hofer@1und1.de>

--- hosts/shared/home-manager/editor/nvim/lua/plugins/markdown-preview.lua
+++ hosts/shared/home-manager/editor/nvim/lua/plugins/markdown-preview.lua
@@ -3,11 +3,30 @@ return {
     "iamcco/markdown-preview.nvim",
     lazy = true,
     ft = { "markdown", "rst" },
-    build = function()
-      vim.fn["mkdp#util#install"]()
-    end,
+    build = ":call mkdp#util#install()",
     config = function()
       vim.g.mkdp_filetypes = { "markdown", "rst" }
     end,
+    keys = {
+      { "<leader>m", "", desc = "+markdown", mode = { "n", "v" } },
+      {
+        "<leader>mm",
+        "<cmd>MarkdownPreviewToggle<CR>",
+        desc = "Toggle preview",
+        mode = { "n", "v" },
+      },
+      {
+        "<leader>mp",
+        "<cmd>MarkdownPreview<CR>",
+        desc = "Start preview",
+        mode = { "n", "v" },
+      },
+      {
+        "<leader>ms",
+        "<cmd>MarkdownPreviewStop<CR>",
+        desc = "Stop preview",
+        mode = { "n", "v" },
+      },
+    },
   },
 }
