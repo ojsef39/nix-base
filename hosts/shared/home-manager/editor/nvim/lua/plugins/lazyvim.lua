e2c83b08bbc47417371a1711469ab7a42c004a49
Josef Hofer <me@jhofer.de>
2025-04-26 17:42:16 +0200

    chore(hm-editor): use catppuccin-macchiato

---
+++ hosts/shared/home-manager/editor/nvim/lua/plugins/lazyvim.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    specs = {
+      {
+        "akinsho/bufferline.nvim",
+        optional = true,
+        opts = function(_, opts)
+          if (vim.g.colors_name or ""):find("catppuccin") then
+            opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
+          end
+        end,
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-macchiato",
+    },
+  },
+}
