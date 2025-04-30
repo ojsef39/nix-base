f9c2f0712468336793583546bc61684b9cd9e4bf
Josef Hofer <me@jhofer.de>
2025-04-30 18:35:58 +0200

    feat(core): add community cache

--- nix/core.nix
+++ nix/core.nix
@@ -1,4 +1,3 @@
-
 { pkgs, ... }:
 
 {
@@ -6,6 +5,12 @@
     settings = {
       # enable flakes globally
       experimental-features = ["nix-command" "flakes"];
+      substituters = [
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
     };
     package = pkgs.nix;
   };
