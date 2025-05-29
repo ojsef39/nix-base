0d1c1e27f41f8e49732ff92e0794caad31dbf23d
Josef Hofer <me@jhofer.de>
2025-05-29 22:58:02 +0200

    chore(hm-editor): update settings for authentik yaml files

--- hosts/shared/home-manager/editor/nvim/lua/plugins/lsp.lua
+++ hosts/shared/home-manager/editor/nvim/lua/plugins/lsp.lua
@@ -1,38 +1,43 @@
-local lspconfig = require("lspconfig")
-
-lspconfig.helm_ls.setup({
-  settings = {
-    ["helm-ls"] = {
-      yamlls = {
-        path = "yaml-language-server",
-      },
-    },
-  },
-})
-
--- custom tags for authentik yaml files
-lspconfig.yamlls.setup({
-  settings = {
-    yaml = {
-      customTags = {
-        "!Condition sequence",
-        "!Context scalar",
-        "!Enumerate sequence",
-        "!Env scalar",
-        "!Find sequence",
-        "!Format sequence",
-        "!If sequence",
-        "!Index scalar",
-        "!KeyOf scalar",
-        "!Value scalar",
-        "!AtIndex scalar",
-      },
-    },
-  },
-})
-
 return {
   {
     "towolf/vim-helm",
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        helm_ls = {
+          settings = {
+            ["helm-ls"] = {
+              yamlls = {
+                path = "yaml-language-server",
+              },
+            },
+          },
+        },
+        yamlls = {
+          settings = {
+            yaml = {
+              customTags = {
+                "!Condition sequence",
+                "!Context scalar",
+                "!Enumerate sequence",
+                "!Env scalar",
+                "!Find sequence",
+                "!Format sequence",
+                "!If sequence",
+                "!Index scalar",
+                "!KeyOf scalar",
+                "!Value scalar",
+                "!AtIndex scalar",
+              },
+              schemas = {
+                ["https://goauthentik.io/blueprints/schema.json"] = "bp-*.yaml",
+              },
+            },
+          },
+        },
+      },
+    },
   },
 }
