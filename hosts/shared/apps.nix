fa6df8c8542f2e377ff369b27f0811122aa3e760
Josef Hofer <me@jhofer.de>
2025-06-12 08:26:14 +0200

    feat(sys-apps): add nix-tree (#155)

--- hosts/shared/apps.nix
+++ hosts/shared/apps.nix
@@ -4,6 +4,7 @@
     # Nix tools #
     nh
     nix-init # https://github.com/nix-community/nix-init
+    nix-tree
     nix-update # https://github.com/Mic92/nix-update
     nixpkgs-review
     update-nix-fetchgit # https://github.com/expipiplus1/update-nix-fetchgit
