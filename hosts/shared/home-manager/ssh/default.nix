9a892176b0f5fdc0bbf3d487c95d0241fa10c590
Josef Hofer <me@jhofer.de>
2025-08-28 14:24:19 +0200

    fix(hm-ssh): deprecations (#190)
    Co-authored-by: Josef Hofer <josef.hofer@1und1.de>

--- hosts/shared/home-manager/ssh/default.nix
+++ hosts/shared/home-manager/ssh/default.nix
@@ -2,13 +2,23 @@
 {
   programs.ssh = {
     enable = lib.mkDefault true;
+    enableDefaultConfig = false;
 
     matchBlocks = {
       "*" = {
+        addKeysToAgent = "yes";
         extraOptions = {
           IdentityAgent = ''"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"'';
           UseKeychain = "yes";
-          AddKeysToAgent = "yes";
+          ForwardAgent = "no";
+          Compression = "no";
+          ServerAliveInterval = "0";
+          ServerAliveCountMax = "3";
+          HashKnownHosts = "no";
+          UserKnownHostsFile = "~/.ssh/known_hosts";
+          ControlMaster = "no";
+          ControlPath = "~/.ssh/master-%r@%n:%p";
+          ControlPersist = "no";
         };
       };
     };
