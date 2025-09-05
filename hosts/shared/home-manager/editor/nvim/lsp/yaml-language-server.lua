ac2f8d1b652ee396019cf8eb035b9f6676ee94b9
Josef Hofer <me@jhofer.de>
2025-09-05 21:24:54 +0200

    yamlls

--- hosts/shared/home-manager/editor/nvim/lsp/yaml-language-server.lua
+++ hosts/shared/home-manager/editor/nvim/lsp/yaml-language-server.lua
@@ -4,26 +4,37 @@ local root = uv.cwd() -- fallback; you might already use a more advanced root fi
 
 -- Try to read `.yaml-ls.json` from the root directory
 local function load_yaml_ls_settings()
-    local path = root .. "/.yaml-ls.json"
-    if fn.filereadable(path) == 1 then
-        local ok, content = pcall(fn.readfile, path)
-        if ok then
-            local json = fn.json_decode(table.concat(content, "\n"))
-            if json then return json end
-        end
-    end
-    return {}
+	local path = root .. "/.yaml-ls.json"
+	if fn.filereadable(path) == 1 then
+		local ok, content = pcall(fn.readfile, path)
+		if ok then
+			local json = fn.json_decode(table.concat(content, "\n"))
+			if json then
+				return json
+			end
+		end
+	end
+	return {}
 end
 
 ---@type vim.lsp.Config
 return {
-    cmd = { "nix-shell", "-p", "nodePackages.yaml-language-server", "--run", "yaml-language-server --stdio" },
-    filetypes = { "yaml", "yml" },
-    root_markers = {
-        "docker-compose.yml",
-        "docker-compose.yaml",
-        ".git",
-        "src",
-    },
-    settings = load_yaml_ls_settings()
+	cmd = { "nix-shell", "-p", "nodePackages.yaml-language-server", "--run", "yaml-language-server --stdio" },
+	filetypes = { "yaml", "yml" },
+	root_markers = {
+		"docker-compose.yml",
+		"docker-compose.yaml",
+		".git",
+		"src",
+	},
+	settings = vim.tbl_deep_extend("force", {
+		yaml = {
+			validate = true,
+			hover = true,
+			completion = true,
+			format = {
+				enable = true,
+			},
+		},
+	}, load_yaml_ls_settings()),
 }
