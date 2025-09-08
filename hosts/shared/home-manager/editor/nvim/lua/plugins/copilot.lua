bb08a7646af6efdfad9f24a3a702acd25edb4c6d
Josef Hofer <josef.hofer@1und1.de>
2025-09-08 21:25:52 +0200

    fix(hm-editor): copilot keymaps

--- hosts/shared/home-manager/editor/nvim/lua/plugins/copilot.lua
+++ hosts/shared/home-manager/editor/nvim/lua/plugins/copilot.lua
@@ -7,14 +7,6 @@ return {
 				enabled = true,
 				auto_trigger = true,
 				debounce = 75,
-				keymap = {
-					accept = "<C-j>",
-					accept_word = "<C-l>",
-					accept_line = "<C-k>",
-					next = "<C-n>",
-					prev = "<C-p>",
-					dismiss = "<C-h>",
-				},
 			},
 			filetypes = {
 				["*"] = true, -- Enable for ALL filetypes by default
@@ -31,5 +23,54 @@ return {
 				return true
 			end,
 		})
+
+		-- Manual keymap setup to override any conflicts
+		vim.keymap.set("i", "<C-j>", function()
+			if require("copilot.suggestion").is_visible() then
+				require("copilot.suggestion").accept()
+			else
+				return "<C-j>"
+			end
+		end, { expr = true, silent = true, desc = "Accept Copilot suggestion" })
+
+		vim.keymap.set("i", "<C-l>", function()
+			if require("copilot.suggestion").is_visible() then
+				require("copilot.suggestion").accept_word()
+			else
+				return "<C-l>"
+			end
+		end, { expr = true, silent = true, desc = "Accept Copilot word" })
+
+		vim.keymap.set("i", "<C-k>", function()
+			if require("copilot.suggestion").is_visible() then
+				require("copilot.suggestion").accept_line()
+			else
+				return "<C-k>"
+			end
+		end, { expr = true, silent = true, desc = "Accept Copilot line" })
+
+		vim.keymap.set("i", "<C-n>", function()
+			if require("copilot.suggestion").is_visible() then
+				require("copilot.suggestion").next()
+			else
+				return "<C-n>"
+			end
+		end, { expr = true, silent = true, desc = "Next Copilot suggestion" })
+
+		vim.keymap.set("i", "<C-p>", function()
+			if require("copilot.suggestion").is_visible() then
+				require("copilot.suggestion").prev()
+			else
+				return "<C-p>"
+			end
+		end, { expr = true, silent = true, desc = "Previous Copilot suggestion" })
+
+		vim.keymap.set("i", "<C-h>", function()
+			if require("copilot.suggestion").is_visible() then
+				require("copilot.suggestion").dismiss()
+			else
+				return "<C-h>"
+			end
+		end, { expr = true, silent = true, desc = "Dismiss Copilot suggestion" })
 	end,
 }
