37be4f5821b19b323632cf7a6d828b5fadfef149
Josef Hofer <josef.hofer@1und1.de>
2025-09-10 15:09:46 +0200

    fix(hm-editor): helm-ls

---
+++ hosts/shared/home-manager/editor/nvim/lua/plugins/helm-ls.lua
@@ -0,0 +1,18 @@
+return {
+	src = "https://github.com/qvalentin/helm-ls.nvim",
+	defer = true,
+	config = function()
+		require("helm-ls").setup({
+			conceal_templates = {
+				-- enable the replacement of templates with virtual text of their current values
+				enabled = true,
+			},
+			indent_hints = {
+				-- enable hints for indent and nindent functions
+				enabled = true,
+				-- show the hints only for the line the cursor is on
+				only_for_current_line = true,
+			},
+		})
+	end,
+}
