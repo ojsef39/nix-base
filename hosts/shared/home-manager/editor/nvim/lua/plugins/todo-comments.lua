78440afa43c770993496ac330ab72acc958b83d8
Josef Hofer <me@jhofer.de>
2025-09-16 11:38:16 +0200

    chore(hm-editor): allow searching for todos in current buffer

--- hosts/shared/home-manager/editor/nvim/lua/plugins/todo-comments.lua
+++ hosts/shared/home-manager/editor/nvim/lua/plugins/todo-comments.lua
@@ -5,32 +5,33 @@ return {
 			-- Default configuration
 		})
 
-		-- Create mini.pick integration for todos using correct API
-		local function todo_picker()
+		-- Create mini.pick integration for todos
+		local function todo_picker(local_only)
 			local search = require("todo-comments.search")
+			local current_file = local_only and vim.api.nvim_buf_get_name(0) or nil
 
 			search.search(function(results)
-				local items = {}
-				local lookup = {}
+				local filtered_results = {}
+				for _, todo in ipairs(results) do
+					if not local_only or todo.filename == current_file then
+						table.insert(filtered_results, todo)
+					end
+				end
 
-				for i, todo in ipairs(results) do
-					local relative_path = vim.fn.fnamemodify(todo.filename, ":.")
-					local display_text = string.format("%s:%d: %s", relative_path, todo.lnum, todo.text)
-					items[i] = display_text
-					lookup[display_text] = {
-						filename = todo.filename,
-						lnum = todo.lnum,
-						col = todo.col or 1,
-					}
+				local items = {}
+				for _, todo in ipairs(filtered_results) do
+					local display_text = local_only and string.format("%d: %s", todo.lnum, todo.text)
+						or string.format("%s:%d: %s", vim.fn.fnamemodify(todo.filename, ":."), todo.lnum, todo.text)
+					table.insert(items, display_text)
 				end
 
 				require("mini.pick").start({
 					source = {
 						items = items,
-						name = "TODOs",
-						choose = function(item)
-							if item and lookup[item] then
-								local todo = lookup[item]
+						name = local_only and "TODOs (Current File)" or "TODOs",
+						choose = function(item_idx)
+							local todo = filtered_results[item_idx]
+							if todo then
 								local target_win = require("mini.pick").get_picker_state().windows.target
 								vim.api.nvim_win_call(target_win, function()
 									vim.cmd("edit " .. vim.fn.fnameescape(todo.filename))
@@ -43,8 +44,36 @@ return {
 			end)
 		end
 
+		-- Create quickfix function for current buffer only
+		local function todo_quickfix_current()
+			local search = require("todo-comments.search")
+			local current_file = vim.api.nvim_buf_get_name(0)
+
+			search.search(function(results)
+				local qf_items = {}
+				for _, todo in ipairs(results) do
+					if todo.filename == current_file then
+						table.insert(qf_items, {
+							filename = todo.filename,
+							lnum = todo.lnum,
+							col = todo.col or 1,
+							text = todo.text,
+						})
+					end
+				end
+				vim.fn.setqflist(qf_items, "r")
+				vim.cmd("copen")
+			end)
+		end
+
 		-- Keymaps
-		vim.keymap.set("n", "<leader>dx", todo_picker, { desc = "Search TODOs" })
+		vim.keymap.set("n", "<leader>dx", function()
+			todo_picker(false)
+		end, { desc = "Search TODOs" })
+		vim.keymap.set("n", "<leader>dX", function()
+			todo_picker(true)
+		end, { desc = "Search TODOs (Current File)" })
 		vim.keymap.set("n", "<leader>dl", "<cmd>TodoQuickFix<cr>", { desc = "TODO List" })
+		vim.keymap.set("n", "<leader>dL", todo_quickfix_current, { desc = "TODO List (Current File)" })
 	end,
 }
