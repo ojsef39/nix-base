79196237347b52a5fdae9efd010e1148ead65f6c
Josef Hofer <josef.hofer@1und1.de>
2025-10-20 12:32:12 +0200

    chore(hm-git): fix deprecations

--- hosts/shared/home-manager/git/default.nix
+++ hosts/shared/home-manager/git/default.nix
@@ -14,10 +14,12 @@
       enable = lib.mkDefault true;
       lfs.enable = lib.mkDefault true;
 
-      userName = "${vars.user.full_name}";
-      userEmail = "${vars.user.email}";
+      settings = {
+        user = {
+          name = "${vars.user.full_name}";
+          email = "${vars.user.email}";
+        };
 
-      extraConfig = {
         init.defaultBranch = "main";
         push.autoSetupRemote = lib.mkDefault true;
         pull.rebase = lib.mkDefault true;
@@ -35,28 +37,29 @@
         };
 
         tag.forceSignAnnotated = lib.mkDefault false;
-      };
 
-      aliases = {
-        mr = "!sh -c 'git fetch $1 merge-requests/$2/head:mr-$1-$2 && git checkout mr-$1-$2' -";
-        br = "branch";
-        co = "checkout";
-        st = "status";
-        ls = "log --pretty=format:\"%C(yellow)%h%Cred%d\\\\ %Creset%s%Cblue\\\\ [%cn]\" --decorate";
-        ll = "log --pretty=format:\"%C(yellow)%h%Cred%d\\\\ %Creset%s%Cblue\\\\ [%cn]\" --decorate --numstat";
-        cm = "commit -m";
-        ca = "commit -am";
-        dc = "diff --cached";
-        amend = "commit --amend -m";
-        update = "submodule update --init --recursive";
-        foreach = "submodule foreach";
-      };
-
-      delta = {
-        enable = lib.mkDefault true;
-        options = {
-          features = "side-by-side";
+        alias = {
+          mr = "!sh -c 'git fetch $1 merge-requests/$2/head:mr-$1-$2 && git checkout mr-$1-$2' -";
+          br = "branch";
+          co = "checkout";
+          st = "status";
+          ls = "log --pretty=format:\"%C(yellow)%h%Cred%d\\\\ %Creset%s%Cblue\\\\ [%cn]\" --decorate";
+          ll = "log --pretty=format:\"%C(yellow)%h%Cred%d\\\\ %Creset%s%Cblue\\\\ [%cn]\" --decorate --numstat";
+          cm = "commit -m";
+          ca = "commit -am";
+          dc = "diff --cached";
+          amend = "commit --amend -m";
+          update = "submodule update --init --recursive";
+          foreach = "submodule foreach";
         };
+      };
+    };
+
+    delta = {
+      enable = lib.mkDefault true;
+      enableGitIntegration = true;
+      options = {
+        features = "side-by-side";
       };
     };
     lazygit = {
