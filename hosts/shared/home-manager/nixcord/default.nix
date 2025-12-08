bbbd98db0c73556a0813995db4d153b35b1776a0
Josef Hofer <me@jhofer.de>
2025-12-08 11:40:29 +0100

    chore(hm-nixcord): update skikiCodeBlock CSS (#259)
    Co-authored-by: Josef Hofer <josef.hofer@1und1.de>

--- hosts/shared/home-manager/nixcord/default.nix
+++ hosts/shared/home-manager/nixcord/default.nix
@@ -103,7 +103,7 @@ in {
         summaries.enable = true;
         shikiCodeblocks = {
           enable = true;
-          theme = "https://raw.githubusercontent.com/shikijs/textmate-grammars-themes/ecd9fb1b4a38381df95048c19cf9b8bdcbb1ec09/packages/tm-themes/themes/catppuccin-macchiato.json";
+          theme = "https://raw.githubusercontent.com/shikijs/textmate-grammars-themes/bc5436518111d87ea58eb56d97b3f9bec30e6b83/packages/tm-themes/themes/catppuccin-macchiato.json";
         };
         translate.enable = true;
         typingIndicator.enable = true;
