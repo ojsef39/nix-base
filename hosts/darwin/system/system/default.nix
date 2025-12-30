7bc3e6b1b3ca41e6174df7630998b54e7c83f064
Josef Hofer <me@jhofer.de>
2025-12-30 12:12:59 +0100

    fix(hm-shell): fish 4.3 doesnt work with fenv

--- hosts/darwin/system/system/default.nix
+++ hosts/darwin/system/system/default.nix
@@ -17,7 +17,10 @@
 {
   # You can enable the fish shell and manage fish configuration and plugins with Home Manager, but to enable vendor fish completions provided by Nixpkgs you
   # will also want to enable the fish shell in /etc/nixos/configuration.nix:
-  programs.fish.enable = true;
+  programs.fish = {
+    enable = true;
+    useBabelfish = true;
+  };
 
   # User configuration (needed for fish to work as default shell: https://github.com/nix-darwin/nix-darwin/issues/1237)
   users = {
