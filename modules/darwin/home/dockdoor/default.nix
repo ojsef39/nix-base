0bf855ee3b44a28b3bfe7ea7841be929c58e5a1d
Josef Hofer <me@jhofer.de>
2026-01-26 18:01:14 +0100

    chore(mod/dw/brew): move dockdoor to modules and set default config

---
+++ modules/darwin/home/dockdoor/default.nix
@@ -0,0 +1,33 @@
+{...}: {
+  # installed via mod/dw/brew
+  targets.darwin.defaults = {
+    "com.ethanbills.DockDoor" = {
+      # Auto-update settings
+      SUAutomaticallyUpdate = 1;
+      SUEnableAutomaticChecks = 1;
+      SUSendProfileInfo = 0;
+
+      # Preview and window behavior
+      allowDynamicImageSizing = 1;
+      enableDockPreviewGestures = 1;
+      enableLivePreview = 0;
+      enableLivePreviewForWindowSwitcher = 1;
+      useEmbeddedDockPreviewElements = 1;
+      openDelay = 0;
+      previewHoverAction = "none";
+      sizingMultiplier = 7;
+
+      # Window switcher settings
+      enableCmdTabEnhancements = 1;
+      enableWindowSwitcherGestures = 1;
+      enableWindowSwitcherSearch = 0;
+      includeHiddenWindowsInSwitcher = 1;
+      windowPreviewSortOrder = "recentlyUsed";
+      hasSeenCmdTabFocusHint = 1;
+
+      # UI elements
+      showActiveAppIndicator = 0;
+      enabledTrafficLightButtons = ["toggleFullScreen" "quit" "close" "minimize"];
+    };
+  };
+}
